2685fb314dfe2976c363d1604a55c15bc71b1cf5
幕希Lin <2103887953@qq.com>
2016-09-21 22:31:00 +0800

    day_3

---
+++ js/swipe.sky.js
@@ -0,0 +1,24 @@
+function SwipeSky (container, className) {
+  //获取背景图
+  var element = container.find(className)
+  //滑动对象
+  var SwipeSky = {}
+  //获取容器尺寸
+  var height = container.height()
+  var width = container.width()
+  //设定背景图尺寸 可能会加载后改变图片尺寸因为是计算的，之后再讨论（也可以嵌套进模板在模板里渲染
+  element.css({
+    height: height + 'px',
+    right: '0px'
+  })
+  //动画移动
+  SwipeSky.scrollTo = function(x, speed) {
+    element.css({
+      'right': '-' + x + 'px',
+      'transition-timing-function' : 'linear',
+      'transition-duration' : speed + 'ms',
+      'transform' : 'translate3d(0px, 0px, 0px)'
+    })
+  }
+  return SwipeSky
+}
